4b239e7627b1d55c93ba213f7c0855137a0ec917
jubhavsar <juhi_bhavsar@yahoo.com>
2021-09-28 17:24:56 -0400

    hello function

---
+++ hello.js
@@ -0,0 +1,29 @@
+// const sayHello(name)
+// {
+//   console.log("Hello",+name);
+// }
+
+// sayhello("juhi");
+
+var sayHello = function(name)
+{
+  //console.log("Hello,"+name);
+  return "Hello, " +name;
+}
+var greet = sayHello('juhi');
+console.log(greet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
