25665fa68bcd6d94b6443f0fb272dfbabe5a3160
jubhavsar <juhi_bhavsar@yahoo.com>
2021-10-27 17:13:47 -0400

    organize folder

---
+++ odd.js
@@ -0,0 +1,6 @@
+const isOdd = function(num)
+{
+  return num % 2 !=0;
+}
+console.log("5 is: "+isOdd(5));
+console.log("50 is odd: "+isOdd(50));
---
+++ tinkering.js
@@ -0,0 +1,9 @@
+const multiply = (a,b)=>a*b;console.log(multiply(5,6)); 
+let city = "Vancouver";
+let yearIncorporated = 1886;
+let currentYear = 2015;
+let age = currentYear - yearIncorporated;
+
+console.log(city + " is " + age + " years old.");
+console.log(`The ${city} is ${age} years old`);
+
